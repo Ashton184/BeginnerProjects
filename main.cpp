fda6f2b756dbaf942854714d0d6353de54de269c
Ashton184 <t.myngoc184@gmail.com>
2024-06-29 22:15:46 -0400

    Add a new project to Git

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int x, y, z, a;
+    cout << "-------------Hello, Welcome to CGPA Calculator!----------" << endl <<"Please submit the following:   "<< endl;
+    cout << "Number of courses taken:    ";
+    cin >> a;
+    for (int i = 1; i < a; i++){
+
+    }
     return 0;
 }

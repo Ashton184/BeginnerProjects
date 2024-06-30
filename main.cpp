5aaa18b97f80bd44035d42e505958d2754edab1a
Ashton184 <t.myngoc184@gmail.com>
2024-06-30 02:07:37 -0400

    Simple CGPA Calculator

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-    int x, y, z, a;
+    float x, y, a;
+    float SumGP = 0, SumCC = 0;
     cout << "-------------Hello, Welcome to CGPA Calculator!----------" << endl <<"Please submit the following:   "<< endl;
-    cout << "Number of courses taken:    ";
+    cout << "Number of courses taken:";
     cin >> a;
-    for (int i = 1; i < a; i++){
-
-    }
+        for (int i = 1; i < a+1; i++){
+            cout << "Grade of Course " << i <<" :";
+            cin >> x;
+            cout <<"Credit Hours of Course " << i <<" :";
+            cin >> y;
+            const float z = x * y;
+            SumGP += z;
+            SumCC += y;
+        }
+    cout << "CGPA: " << fixed << setprecision(2)<<SumGP/SumCC;
     return 0;
 }

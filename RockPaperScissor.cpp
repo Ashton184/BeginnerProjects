6696f89fd6cdfb111ddcc09dbfda87c5dca7c601
Ashton184 <t.myngoc184@gmail.com>
2024-07-04 22:44:41 -0400

    Updated

--- RockPaperScissor.cpp
+++ RockPaperScissor.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main()
-{
+int main() {
+    string hand;
+    cout << "Hello! Let's play a Rock, Paper and Scissor with me!" << endl;
+
+    cout << "Choose one: " << endl;
+    cout<< "1: Rock" << endl << "2: Paper" << endl <<"3: Scissor";
+    cin >> hand;
+
+    srand(time(NULL));
+    int Comp = (rand() % 3) +1;
+    switch (Comp)
+    {
+    case 1: return 'Rock';
+    case 2: return 'Paper';
+    case 3: return 'Scissor';
+    default: return 0;
+    }
 
 }

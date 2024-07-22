5915f98ad6dcc7754ac5561801c5cf0d47eac01d
Ashton184 <t.myngoc184@gmail.com>
2024-07-22 15:52:31 -0400

    Updated

--- RockPaperScissor.cpp
+++ RockPaperScissor.cpp
@@ -3,22 +3,77 @@
 #include <ctime>
 using namespace std;
 
-int main() {
-    string hand;
-    cout << "Hello! Let's play a Rock, Paper and Scissor with me!" << endl;
-
-    cout << "Choose one: " << endl;
-    cout<< "1: Rock" << endl << "2: Paper" << endl <<"3: Scissor";
-    cin >> hand;
-
+char Computer() {
+    int move;
     srand(time(NULL));
-    int Comp = (rand() % 3) +1;
-    switch (Comp)
+    move = rand() % 3;
+    if (move == 0)
     {
-    case 1: return 'Rock';
-    case 2: return 'Paper';
-    case 3: return 'Scissor';
-    default: return 0;
+        return 'p';
     }
+    if (move == 1)
+    {
+        return 's';
+    }
+    return 'r';
+}
 
+int Result(char PlayerMove, char ComputerMove) {
+    if (PlayerMove == ComputerMove)
+    {
+        return 0;
+    }
+    if (PlayerMove == 's' && ComputerMove == 'r')
+    {
+        return -1;
+    }
+    if (PlayerMove == 's' && ComputerMove == 'p')
+    {
+        return 1;
+    }
+    if (PlayerMove == 'r' && ComputerMove == 'p')
+    {
+        return -1;
+    }
+    if (PlayerMove == 'r' && ComputerMove == 's')
+    {
+        return 1;
+    }
+    if (PlayerMove == 'p' && ComputerMove == 'r')
+    {
+        return 1;
+    }
+    if (PlayerMove == 'p' && ComputerMove == 's')
+    {
+        return -1;
+    }
+    return 0;
+}
+int main() {
+    char PlayerMove;
+    cout <<" -------Welcome-------" << endl;
+    cout<<"Let's play Rock, Scissors, Paper!"<< endl;
+    cout<<"Please choose one: "<< endl;
+    cout<<"r: ROCK" << endl <<"p: PAPER" << endl <<"s: SCISSORS"<< endl;
+    cin >> PlayerMove;
+    while (PlayerMove != 'r' && PlayerMove != 'p' && PlayerMove != 's')
+    {
+        cout <<"Invalid Input! Please try again";
+        cin >> PlayerMove;
+    }
+    char ComputerMove = Computer();
+    int result = Result(PlayerMove, ComputerMove);
+    if (result == 0)
+    {
+        cout<<"Draw!";
+    }
+    else if (result == 1)
+    {
+        cout << "You Win!";
+    }
+    else
+    {
+        cout << "Computer Win!";
+    }
+    return 0;
 }
